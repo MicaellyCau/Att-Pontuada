1434b27835e857009b79ae70e8ecf39debbb6b0a
MicaellyCau <160753449+MicaellyCau@users.noreply.github.com>
2024-09-10 11:43:46 -0300

    Update Pilha.java

--- PilhaEFila/src/Questao4/Pilha.java
+++ PilhaEFila/src/Questao4/Pilha.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Pilha {
-	  public static boolean ehPalindromo(String nome) {
+	  public static boolean palindromo(String nome) {
 	        Stack<Character> pilha = new Stack<>();
 	        String n = nome.replaceAll("", "").toLowerCase();
 	        int tamanho = n.length();
@@ -34,7 +34,7 @@ public class Pilha {
 	        System.out.println("Digite uma palavra: ");
 	        String palavra = scanner.nextLine();
 	        
-	        if (ehPalindromo(palavra)) {
+	        if (palindromo(palavra)) {
 	            System.out.println("A palavra é um palíndromo.");
 	        } else {
 	            System.out.println("A palavra não é um palíndromo.");
